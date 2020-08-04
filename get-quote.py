76f137095bbd2a9ce323a321a2b376621c09345a
JRTitor <68708953+JRTitor@users.noreply.github.com>
2020-08-04 23:13:41 +0300

    Update get-quote.py
    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -4,8 +4,11 @@ def main():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
 
-  print(quotes)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
